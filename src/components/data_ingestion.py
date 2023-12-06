9a038b6dccd5ed1db0332fd7af5b3f9ecc9e4d1b
Kuljot Singh <kuljotme035@gmail.com>
2023-12-06 14:58:39 +0530

    Model trainer added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,5 @@
 import os
 import sys
-print(sys.path)
 from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
@@ -10,7 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path:str =os.path.join('artifacts',"train.csv")
@@ -49,4 +49,6 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()    
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
